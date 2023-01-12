96262fe0d409146fd703758954d5e8ccd058c3dd
Erik Perri <erik.perri@gmail.com>
2023-01-12 08:12:08 -0500

    build: Moved sample check to node script for consistency in types of scripts used

---
+++ samples/check.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { execSync } = require('child_process');
+
+console.log(execSync(`npm run generate-samples`, { encoding: 'utf-8' }));
+
+const currentStatus = execSync('git status --porcelain');
+if (!currentStatus.length) {
+  process.exit(0);
+}
+
+console.log('Git changes detected after generating samples.');
+console.log(currentStatus.toString());
+process.exit(1);
