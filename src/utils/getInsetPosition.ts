44b80d51e298de15cd8f95ff61a7c6433384ae93
Erik Perri <erik.perri@gmail.com>
2023-01-13 08:07:22 -0500

    refactor: Cleanup

--- src/utils/getInsetPosition.ts
+++ src/utils/getInsetPosition.ts
@@ -1,16 +1,21 @@
 import { IMagickImage } from '@imagemagick/magick-wasm';
 
+const PIXEL_ALPHA_INDEX = 3;
+
 export default function getInsetPosition(
   image: IMagickImage,
-  opacityCutoff: number
+  alphaCutoff: number
 ) {
+  // If we make it to the center of the image we can cancel out since the inset
+  // is expected to be equal on both sides
+  const maxLineX = Math.floor(image.width / 2) - 1;
   const centerLineY = image.height / 2;
   let insetPosition = 0;
 
   image.getPixels((pixels) => {
-    for (let x = 0; x < Math.floor(image.width / 3); x++) {
+    for (let x = 0; x < maxLineX; x++) {
       const pixel = pixels.getPixel(x, centerLineY);
-      if (pixel[3] >= opacityCutoff) {
+      if (pixel[PIXEL_ALPHA_INDEX] >= alphaCutoff) {
         insetPosition = x;
         break;
       }
