536d342ba139dae177103098cb5784b3068b71ab
Erik Perri <erik.perri@gmail.com>
2023-01-13 23:54:35 -0500

    build: Switched generator to all files in input for easier testing

--- samples/generate.js
+++ samples/generate.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const { lstatSync, readdirSync } = require('fs');
+const { resolve, join } = require('path');
 
-['ic_launcher-xxxhdpi.png', 'ic_launcher_round-xxxhdpi.png'].forEach(
-  (image) => {
+const inputPath = resolve(__dirname, 'input');
+const outputPath = resolve(__dirname, 'output');
+
+readdirSync(inputPath)
+  .filter((file) => lstatSync(join(inputPath, file)).isFile())
+  .forEach((file) => {
+    const input = join(inputPath, file);
+    const output = join(outputPath, file);
     console.log(
-      execSync(
-        `node bin/add-badge.js "samples/input/${image}" "samples/output/${image}" "ALPHA"`,
-        { encoding: 'utf-8' }
-      )
+      execSync(`node bin/add-badge.js "${input}" "${output}" "ALPHA"`, {
+        encoding: 'utf-8',
+      }).trimEnd()
     );
-  }
-);
+  });
