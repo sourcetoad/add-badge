fc270582fb134871e7355df763f445d99da10cf1
Erik Perri <erik.perri@gmail.com>
2023-01-21 15:14:35 -0500

    update: Added get inset at gravity helper

---
+++ __tests__/utils/getInsetAtGravity.spec.ts
@@ -0,0 +1,127 @@
+import { Gravity, ImageMagick } from '@imagemagick/magick-wasm';
+import * as fs from 'fs';
+import { describe, expect, it } from 'vitest';
+
+import getInsetAtGravity, {
+  HIGHEST_ANDROID_SHADOW_ALPHA,
+} from '../../src/utils/getInsetAtGravity';
+
+describe('getInsetAtGravity', () => {
+  it.each([
+    // region ic_launcher-xxxhdpi.png
+    [
+      'ic_launcher-xxxhdpi.png Northwest',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.Northwest,
+      33,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png North',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.North,
+      20,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png Northeast',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.Northeast,
+      33,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png West',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.West,
+      19,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png East',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.East,
+      19,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png Southwest',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.Southwest,
+      33,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png South',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.South,
+      19,
+    ],
+    [
+      'ic_launcher-xxxhdpi.png Southeast',
+      'ic_launcher-xxxhdpi.png',
+      Gravity.Southeast,
+      33,
+    ],
+    // endregion
+    // region ic_launcher_round-xxxhdpi.png
+    [
+      'ic_launcher_round-xxxhdpi.png Northwest',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.Northwest,
+      47,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png North',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.North,
+      7,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png Northeast',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.Northeast,
+      47,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png West',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.West,
+      7,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png East',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.East,
+      7,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png Southwest',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.Southwest,
+      47,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png South',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.South,
+      7,
+    ],
+    [
+      'ic_launcher_round-xxxhdpi.png Southeast',
+      'ic_launcher_round-xxxhdpi.png',
+      Gravity.Southeast,
+      47,
+    ],
+    // endregion
+  ])('works as expected with %s', async (_, file, gravity, expected) => {
+    let result: number | undefined = undefined;
+
+    await ImageMagick.read(
+      fs.readFileSync(`samples/input/${file}`),
+      (image) => {
+        result = getInsetAtGravity(
+          image,
+          gravity,
+          HIGHEST_ANDROID_SHADOW_ALPHA,
+        );
+      },
+    );
+
+    expect(result).toEqual(expected);
+  });
+});
---
+++ src/utils/getInsetAtGravity.ts
@@ -0,0 +1,126 @@
+import {
+  Gravity,
+  IMagickImage,
+  VirtualPixelMethod,
+} from '@imagemagick/magick-wasm';
+
+const PIXEL_ALPHA_INDEX = 3;
+export const HIGHEST_ANDROID_SHADOW_ALPHA = 52;
+
+export default function getInsetAtGravity(
+  image: IMagickImage,
+  gravity: Gravity,
+  alphaCutoff: number,
+): number {
+  // Change the virtual pixel method to transparent before checking the pixels.
+  // Without this some older images are reporting areas that are opaque as
+  // semi-transparent and causing the wrong inset to be detected.
+  const previousMethod = image.virtualPixelMethod;
+  image.virtualPixelMethod = VirtualPixelMethod.Transparent;
+
+  let insetPosition = 0;
+  const centerPointX = Math.floor(image.width / 2);
+  const centerPointY = Math.floor(image.height / 2);
+
+  image.getPixels((pixels) => {
+    switch (gravity) {
+      case Gravity.Northwest:
+        for (let x = 0, y = 0; x < centerPointX && y < centerPointY; x++, y++) {
+          const pixel = pixels.getPixel(x, y);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = Math.round(Math.hypot(x, y));
+            break;
+          }
+        }
+        break;
+
+      case Gravity.North:
+        for (let y = 0; y < centerPointY; y++) {
+          const pixel = pixels.getPixel(centerPointX, y);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = y;
+            break;
+          }
+        }
+        break;
+
+      case Gravity.Northeast:
+        for (
+          let x = image.width - 1, y = 0;
+          x > centerPointX && y < centerPointY;
+          x--, y++
+        ) {
+          const pixel = pixels.getPixel(x, y);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = Math.round(Math.hypot(image.width - 1 - x, y));
+            break;
+          }
+        }
+        break;
+
+      case Gravity.West:
+        for (let x = image.width - 1; x > centerPointX; x--) {
+          const pixel = pixels.getPixel(x, centerPointY);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = image.width - 1 - x;
+            break;
+          }
+        }
+        break;
+
+      case Gravity.East:
+        for (let x = 0; x < centerPointX; x++) {
+          const pixel = pixels.getPixel(x, centerPointY);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = x;
+            break;
+          }
+        }
+        break;
+
+      case Gravity.Southwest:
+        for (
+          let x = 0, y = image.height - 1;
+          x < centerPointX && y > centerPointY;
+          x++, y--
+        ) {
+          const pixel = pixels.getPixel(x, y);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = Math.round(Math.hypot(x, image.height - 1 - y));
+            break;
+          }
+        }
+        break;
+
+      case Gravity.South:
+        for (let y = image.height - 1; y > centerPointY; y--) {
+          const pixel = pixels.getPixel(centerPointX, y);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = image.height - 1 - y;
+            break;
+          }
+        }
+        break;
+
+      case Gravity.Southeast:
+        for (
+          let x = image.width - 1, y = image.height - 1;
+          x > centerPointX && y > centerPointY;
+          x--, y--
+        ) {
+          const pixel = pixels.getPixel(x, y);
+          if (pixel[PIXEL_ALPHA_INDEX] > alphaCutoff) {
+            insetPosition = Math.round(
+              Math.hypot(image.width - 1 - x, image.height - 1 - y),
+            );
+            break;
+          }
+        }
+        break;
+    }
+  });
+
+  image.virtualPixelMethod = previousMethod;
+
+  return insetPosition;
+}
