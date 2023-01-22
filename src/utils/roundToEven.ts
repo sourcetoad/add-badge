aebe5179ca94fca6b43f29274230455f614e6749
Erik Perri <erik.perri@gmail.com>
2023-01-21 23:17:53 -0500

    docs: Added description of why we round to even

--- src/utils/roundToEven.ts
+++ src/utils/roundToEven.ts
@@ -1,3 +1,9 @@
+/**
+ * When dealing with sizes as small as a badge on an icon, 1 pixel being off
+ * when centering is noticeable as not centered. Due to that we round sizes
+ * related to the badge to round numbers, so we can center the text
+ * consistently.
+ */
 export default function roundToEven(value: number): number {
   return 2 * Math.round(value / 2);
 }
