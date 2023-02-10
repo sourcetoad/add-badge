6def32a6ae58daf891c3906079a3c10a85870586
Erik Perri <erik.perri@gmail.com>
2023-02-09 20:55:51 -0500

    test: Added `require` to allow ImageMagick to be initialized during our tests

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,8 +1,10 @@
 import { initializeImageMagick } from '@imagemagick/magick-wasm';
+import { createRequire } from 'module';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 global.__filename = fileURLToPath(import.meta.url);
 global.__dirname = dirname(__filename);
+global.require = createRequire(import.meta.url);
 
 await initializeImageMagick();
