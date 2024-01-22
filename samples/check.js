39408193c1062f84d9f19e2b3089e90d6ecc2d97
Erik Perri <erik.perri@gmail.com>
2024-01-22 08:20:17 -0500

    build: Cleared output before generation in check script to catch potentially removed samples being left around

--- samples/check.js
+++ samples/check.js
@@ -2,11 +2,19 @@
 /* eslint-disable no-console */
 
 const { execSync } = require('child_process');
+const { readdirSync, unlinkSync } = require('fs');
 
+console.log('Removing samples...');
+readdirSync('samples/output').forEach((file) =>
+  unlinkSync(`samples/output/${file}`),
+);
+
+console.log('Generating samples...');
 console.log(execSync(`npm run generate-samples`, { encoding: 'utf-8' }));
 
 const currentStatus = execSync('git status --porcelain');
 if (!currentStatus.length) {
+  console.log('No changes detected after generating samples.');
   process.exit(0);
 }
 
