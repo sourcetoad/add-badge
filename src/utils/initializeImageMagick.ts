ef038288791ce1f0f16e522f448eb7bd58c43df9
Erik Perri <erik.perri@sourcetoad.com>
2024-05-08 13:03:07 -0400

    fix: fixed wasm path resolution

--- src/utils/initializeImageMagick.ts
+++ src/utils/initializeImageMagick.ts
@@ -1,11 +1,10 @@
 import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
 
 import { initializeImageMagick as baseInitializeImageMagick } from '@imagemagick/magick-wasm';
 
 export default async function initializeImageMagick(): Promise<void> {
   const wasmBytes = readFileSync(
-    resolve('node_modules/@imagemagick/magick-wasm/dist/magick.wasm'),
+    require.resolve('@imagemagick/magick-wasm/magick.wasm'),
   );
   await baseInitializeImageMagick(wasmBytes);
 }
