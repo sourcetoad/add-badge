52499e1972ab3272a245240d59ab550b9288382f
Erik Perri <erik.perri@gmail.com>
2024-07-15 22:09:33 -0400

    test: switched to fixed resolve pattern in test setup

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { createRequire } from 'node:module';
-import { dirname, resolve } from 'node:path';
+import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { initializeImageMagick } from '@imagemagick/magick-wasm';
@@ -10,6 +10,6 @@ global.__dirname = dirname(__filename);
 global.require = createRequire(import.meta.url);
 
 const wasmBytes = readFileSync(
-  resolve('node_modules/@imagemagick/magick-wasm/dist/magick.wasm'),
+  require.resolve('@imagemagick/magick-wasm/magick.wasm'),
 );
 await initializeImageMagick(wasmBytes);
